4304af2b504fca58d1e67631992e36629d6a3d0a
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-02-11 23:46:00 -0700

    added transition for scroll on header, added image swap on dbl click, and added border change for image hover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
-// Your code goes here
+const dashedBorder = "2px dashed #C0C0C0"
+
+const busImage = document.querySelector("#bus-img");
+busImage.addEventListener(("mouseover"), function(event) {
+    busImage.style.borderBottom = dashedBorder;
+    busImage.style.borderLeft = dashedBorder;
+    busImage.style.borderRight = dashedBorder;
+});
+busImage.addEventListener(("mouseleave"), function(event) {
+    busImage.style.border = "0";
+});
+let isBlue = false;
+busImage.addEventListener(("dblclick"), function(event) {
+    if (isBlue === false) {
+        busImage.setAttribute("src", "img/fun-bus-blue.jpg");
+        isBlue = true;
+    }
+    else {
+        busImage.setAttribute("src", "img/fun-bus-yellow.jpg");
+        isBlue = false;
+    }
+    console.log(event);
+});
+
+const header = document.querySelector("header");
+document.addEventListener("scroll", function(event) {
+    if (window.scrollY > 50) {
+        header.style.backgroundColor = "deepskyblue";
+        header.style.transition = "all .5s";
+    }
+    else {
+        header.style.backgroundColor = "white";
+    }
+    console.log(window.scrollY);
+})
