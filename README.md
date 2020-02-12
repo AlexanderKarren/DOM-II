8e2d199191b327be0d04bfa3c7a9c0ec5913e126
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-02-12 10:02:01 -0700

    added form

--- README.md
+++ README.md
@@ -40,10 +40,12 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 	* [ ] `drag / drop`
 	* [ ] `load`
 	* [ ] `focus`
-	* [ ] `resize`
+	* [x] `resize`
 	* [x] `scroll`
 	* [ ] `select`
 	* [x] `dblclick`
+	* [x] `mouseleave`
+	* [x] `click`
 
 Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
 
--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
 const dashedBorder = "2px dashed #C0C0C0"
 
+// mouseover
 const busImage = document.querySelector("#bus-img");
 busImage.addEventListener(("mouseover"), function(event) {
     busImage.style.borderBottom = dashedBorder;
     busImage.style.borderLeft = dashedBorder;
     busImage.style.borderRight = dashedBorder;
 });
+// mouseleave
 busImage.addEventListener(("mouseleave"), function(event) {
     busImage.style.border = "0";
 });
+
+// dblclick
 let isBlue = false;
 busImage.addEventListener(("dblclick"), function(event) {
     if (isBlue === false) {
@@ -19,9 +23,9 @@ busImage.addEventListener(("dblclick"), function(event) {
         busImage.setAttribute("src", "img/fun-bus-yellow.jpg");
         isBlue = false;
     }
-    console.log(event);
 });
 
+// scroll
 const header = document.querySelector("header");
 document.addEventListener("scroll", function(event) {
     if (window.scrollY > 50) {
@@ -31,5 +35,56 @@ document.addEventListener("scroll", function(event) {
     else {
         header.style.backgroundColor = "white";
     }
-    console.log(window.scrollY);
 })
+
+// click
+document.querySelectorAll("h2").forEach(function(element) {
+    element.addEventListener("click", function(event) {
+        console.log("text selected");
+    })
+});
+
+// resize
+const body = document.querySelector("body");
+const warning = document.querySelector("#warning");
+const originalWarning = warning.textContent;
+window.addEventListener("resize", function(event) {
+    if (window.screenX > 0) {
+      body.style.backgroundColor = "lightcoral";
+      warning.textContent = "OH NO!"
+    }
+    else {
+        body.style.backgroundColor = "white";
+        warning.textContent = originalWarning;
+    }
+});
+
+// form
+const form = document.querySelector("form");
+const nameInput = document.querySelector("[name=Name]");
+const textArea = document.querySelector("textarea");
+const submitButton = document.querySelector("[type=submit]");
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    if (nameInput.value.length == 0) {
+        const errorMessage = document.createElement("p");
+        errorMessage.textContent = "Fill it out!";
+        errorMessage.style.color = "red";
+        form.appendChild(errorMessage);
+    }
+    else {
+        form.style.display = "none";
+        const doneMessage = document.createElement("p");
+        doneMessage.textContent = `Thank you, ${nameInput.value}, for filling out the form.`
+        form.parentElement.appendChild(doneMessage);
+    }
+});
+
+// stopPropagation()
+
+const mainHeading = document.querySelector("h1");
+
+mainHeading.addEventListener("click", function(event) {
+    mainHeading.color = "blue";
+    console.log("asshole");
+});
